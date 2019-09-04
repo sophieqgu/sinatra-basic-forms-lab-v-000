cd4cafadaa2fdccf4868e8cd9c6bd719ae557a1b
Sophie Gu <sophieqgu@gmail.com>
2019-09-04 03:23:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do 
-    erb :
+    erb :index 
+  end 
+  
+  get '/create_puppy'
+    erb :create_puppy 
+  end 
+  
+  post 'display_puppy'
+    @puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
+    erb :display_puppy 
+  end 
+    
 end
