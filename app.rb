d83dde740922533d1a407966e2e433f3e44894bd
Sophie Gu <sophieqgu@gmail.com>
2019-09-04 03:50:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require_relative 'config/environment'
+require_relative 'models/puppy'
 
 class App < Sinatra::Base
-
+  get '/' do 
+    erb :index 
+  end 
+  
+  get '/new' do 
+    erb :create_puppy 
+  end 
+  
+  post '/puppy' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    @puppy = Puppy.new(@name, @breed, @age)
+    erb :display_puppy 
+  end 
 end
---
+++ models/puppy.rb
@@ -0,0 +1,10 @@
+class Puppy
+  attr_accessor :name, :breed, :months_old
+  
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed 
+    @months_old = age
+  end 
+
+end 
