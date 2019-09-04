d3d088eb3b97e02f92a0f53fa1e5665d4a860ac2
Sophie Gu <sophieqgu@gmail.com>
2019-09-04 03:26:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     erb :create_puppy 
   end 
   
-  post 'display_puppy'
+  post '/display_puppy'
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy 
   end 
