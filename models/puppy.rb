bd305976149bfe0c48ce3df6cc0ff13cc44597ec
Sophie Gu <sophieqgu@gmail.com>
2019-09-04 03:24:53 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,9 +1,9 @@
 class Puppy
   attr_accessor :name, :breed, :months_old
   
-  def initialize(name, breed, months_old)
+  def initialize(name, breed, age)
     @name = name
     @breed = breed 
-    @months_old = months_old
+    @months_old = age
   end 
 end 
